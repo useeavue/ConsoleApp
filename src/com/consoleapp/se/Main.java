9977c514cf7963eaf1b4af09fdad3f5514243e3f
useeavue <therealbrookebond@gmail.com>
2021-08-09 20:55:22 +0400

    initial commit

---
+++ src/com/consoleapp/se/Main.java
@@ -0,0 +1,11 @@
+package com.consoleapp.se;
+
+import com.consoleapp.se.userinput.UserInput;
+
+public class Main {
+    static UserInput in = new UserInput();
+
+    public static void main(String[] args) {
+        in.inputHandle();
+    }
+}
---
+++ src/com/consoleapp/se/point/Point.java
@@ -0,0 +1,24 @@
+package com.consoleapp.se.point;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.x + ", " + this.y + ")";
+    }
+}
